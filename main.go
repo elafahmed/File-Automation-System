2252b7218e2719c4c74327ccd20fbcd55fb67925
elafAhmed <mbarghash666@gmail.com>
2024-07-12 02:50:59 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
-var N string = "Novels"
-var ph string = "Philosophy"
-var H string = "History"
-var oop string = "oop"
-var DSA string = "Data Structure"
-
-func scanPdf() {
-
-}
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func main() {
 
+	srcDir := "E:/semester 2/m213"
+	destDir := "E:/book/oop"
+
+	if _, err := os.Stat(destDir); os.IsNotExist(err) {
+		os.MkdirAll(destDir, os.ModePerm)
+	}
+
+	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && strings.HasSuffix(info.Name(), "oop.pdf") {
+			oldPath := path
+			newPath := filepath.Join(destDir, info.Name())
+			err := os.Rename(oldPath, newPath)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Moved: %s -> %s\n", oldPath, newPath)
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PDF files ending with 'oop' moved successfully!")
 }
-hi kemooooooooo
